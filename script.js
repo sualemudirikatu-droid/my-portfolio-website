2c249690fe2d7361266608efb5254761d1c3680d
sualemudirikatu-droid <sualemudrika@gmail.com>
2026-09-12 07:49:44 +0000

    Fixed mobile navigation menu

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 // Mobile navigation menu
 const menuToggle = document.querySelector(".menu-toggle");
-
 const navLinks = document.querySelector(".nav-links");
 
-
 menuToggle.addEventListener("click", function () {
-
     navLinks.classList.toggle("active");
-
 });
 
-navLinks.classList.toggle("active");
 const navigationLinks = document.querySelectorAll(".nav-links a");
 
-
 navigationLinks.forEach(function (link) {
-
     link.addEventListener("click", function () {
-
         navLinks.classList.remove("active");
-
     });
-
 });
